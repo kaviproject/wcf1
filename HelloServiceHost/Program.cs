917264270fe2eed57d507a2de3e4d4a5b324083a
kaprupati <kaviproject1@gmail.com>
2014-10-08 20:23:04 -0400

    aa

---
+++ HelloServiceHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+         static void Main()
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+                Console.WriteLine("Host Strted at  @" + DateTime.Now.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
